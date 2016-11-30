8eeaa18b9ae59a0678546260d912948224eb1f13
mwconnors <mwconnors@wisc.edu>
2016-11-29 20:20:19 -0600

    Add files via upload

---
+++ normalize.m
@@ -0,0 +1,31 @@
+function [ normalized ] = normalize( original )
+%UNTITLED5 Summary of this function goes here
+%   Detailed explanation goes here
+
+% Normalizing values   
+    l_R = original(:,:,1);
+    l_G = original(:,:,2);
+    l_B = original(:,:,3);
+
+    minM = min(min(l_R));
+    maxM = max(max(l_R));
+    normBWD = (l_R - minM)./(maxM - minM);
+    epNormBWD = normBWD + 0.001;
+    l_R = epNormBWD;
+
+    minM = min(min(l_G));
+    maxM = max(max(l_G));
+    normBWD = (l_G - minM)./(maxM - minM);
+    epNormBWD = normBWD + 0.001;
+    l_G = epNormBWD;
+    
+    minM = min(min(l_B));
+    maxM = max(max(l_B));
+    normBWD = (l_B - minM)./(maxM - minM);
+    epNormBWD = normBWD + 0.001;
+    l_B = epNormBWD;
+    
+    normalized = cat(3, l_R, l_G, l_B);
+
+end
+
--- pyramidBlend.m
+++ pyramidBlend.m
@@ -33,85 +33,25 @@ im2_l2 = laplacianPyramid(im2_g2, im2_g3);
 %   images. Right now, it is split down the center, though later it would
 %   be better to find this automatically using seam finding!
 
-% R = 255*ones(size(im_in1)); %assuming im1_l1 ad im2_l2 still same size
-% m2 = size(im_in1,2);
-% R = cat(2,0*R(:,1:int16(m2/2),:),R(:,int16(m2/2)+1:m2,:));
-% R_g = impyramid(impyramid(impyramid(R, 'reduce'), 'reduce'), 'expand');
-
+%************MOVED TO CREATEMATTE
 R = ones(size(im_in1)); %assuming im1_l1 ad im2_l2 still same size
 m2 = size(im_in1,2);
 R = cat(2,0*R(:,1:(m2/2),:),R(:,(m2/2)+1:m2,:));
 R_g = impyramid(impyramid(impyramid(R, 'reduce'), 'reduce'), 'expand');
+% R_g = createMatte(im1_l1, im2_l1);
 
-%R_g1 = impyramid(impyramid(impyramid(impyramid(R, 'reduce'), 'reduce'), 'reduce'), 'expand');
 R_g1 = impyramid(impyramid(impyramid(R_g, 'reduce'), 'reduce'), 'expand');
+
 %3. Form a combined pyramid LS from L1 and L2 using nodes of GR
 %   as weights:
-% R_g = (R_g);
-% result_l = R_g.*im1_l1+(255-R_g).*im2_l1;
+result_l = normalize(R_g.*im1_l1+(1-R_g).*im2_l1);
+result_l1 = normalize(R_g1.*im1_l2+(1-R_g1).*im2_l2);
 
-result_l = R_g.*im1_l1+(1-R_g).*im2_l1;
-
-% Normalizing values   
-    l_R = result_l(:,:,1);
-    l_G = result_l(:,:,2);
-    l_B = result_l(:,:,3);
-
-    minM = min(min(l_R));
-    maxM = max(max(l_R));
-    normBWD = (l_R - minM)./(maxM - minM);
-    epNormBWD = normBWD + 0.001;
-    l_R = epNormBWD;
-
-    minM = min(min(l_G));
-    maxM = max(max(l_G));
-    normBWD = (l_G - minM)./(maxM - minM);
-    epNormBWD = normBWD + 0.001;
-    l_G = epNormBWD;
-    
-    minM = min(min(l_B));
-    maxM = max(max(l_B));
-    normBWD = (l_B - minM)./(maxM - minM);
-    epNormBWD = normBWD + 0.001;
-    l_B = epNormBWD;
-    
-    result_l = cat(3, l_R, l_G, l_B);
-    
-    %***************3rd Layer, probably need a loop for this section
-    
-    result_l1 = R_g1.*im1_l2+(1-R_g1).*im2_l2;
-    l_R = result_l1(:,:,1);
-    l_G = result_l1(:,:,2);
-    l_B = result_l1(:,:,3);
-
-    minM = min(min(l_R));
-    maxM = max(max(l_R));
-    normBWD = (l_R - minM)./(maxM - minM);
-    epNormBWD = normBWD + 0.001;
-    l_R = epNormBWD;
-
-    minM = min(min(l_G));
-    maxM = max(max(l_G));
-    normBWD = (l_G - minM)./(maxM - minM);
-    epNormBWD = normBWD + 0.001;
-    l_G = epNormBWD;
-    
-    minM = min(min(l_B));
-    maxM = max(max(l_B));
-    normBWD = (l_B - minM)./(maxM - minM);
-    epNormBWD = normBWD + 0.001;
-    l_B = epNormBWD;
-    
-    result_l1 = cat(3, l_R, l_G, l_B);
-    
-    %**************
     
     R_g2 = impyramid(impyramid(impyramid(R_g1, 'reduce'), 'reduce'), 'expand');
     R_g3 = impyramid(impyramid(impyramid(R_g2, 'reduce'), 'reduce'), 'expand');
     R_g4 = impyramid(impyramid(impyramid(R_g3, 'reduce'), 'reduce'), 'expand');
     result_g3 = R_g2.*im1_g3+(1-R_g2).*im2_g3;
-%     result_g2 = R_g3.*im1_g2+(1-R_g3).*im2_g2;
-%     result_g1 = R_g4.*im1_g1+(1-R_g4).*im2_g1;
     
 %4. Collapse the LS pyramid to get the final blended image
 
